c1c9b05ea6d90a96dac15e17d9f462a75544c5b6
Yiki Su <sueyi@student.ubc.ca>
2020-12-22 19:42:09 -0800

    start the eda markdown file

--- script/eda.R
+++ script/eda.R
@@ -1,35 +1,20 @@
 # author: Zhanyi Su
 # date: 2020-12-22
 
-"This script takes in a csv file from the data folder and process the data wrangling. At the end, save the processed data in a feather file in the result folder.
+"This script takes in a feather file from the result folder and generate plots. At the end, save the plot data in a png file in the result folder.
 
-Usage: clean_data.R --input=<input> --out_path=<out_path>
+Usage: eda.R --input=<input> --out_path=<out_path>
 
 Options:
 ----input=<input>        The csv file to read in
 --out_path=<out_path>    Path of where to locally save the feather file
 " -> doc
 
-library(docopt)
-library(tidyverse)
-library(feather)
-library(janitor)
+
 
 opt <- docopt(doc)
 
 main <- function(input, out_path) {
-  
-  # To read in the raw data csv file 
-  raw_data <- read_csv(input)
-  
-  # data wrangling
-  data <- clean_names(raw_data)
-  
-  # write processed data to a feather file
-  try({
-    dir.create(out_path)
-  })
-  write_feather(data, paste0(out_path, "/data.feather"))
   
   
 }
